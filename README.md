125ba8935c3882cf7e236f9a2c6a5a4dd77f8199
Andrei Maximov <andrei.maximov@gmail.com>
2015-07-25 17:41:22 -0700

    Finish BFS: Shortest Reach challenge.

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ This is a collection of some of my [HackerRank](https://www.hackerrank.com) chal
 1. [Algorithms](/algorithms)
   - [Dynamic Programming](/algorithms/dynamic-programming)
     * [Fibonacci Modified](/algorithms/dynamic-programming/fibonacci-modified)
+  - [Graph Theory](/algorithms/graph-theory)
+    * [BFS: Shortest Reach](/algorithms/graph-theory/bfs-shortest-reach)
   - [Search](/algorithms/search)
     * [Cut the Tree](/algorithms/search/cut-the-tree)
     * [Missing Numbers](/algorithms/search/missing-numbers)
---
+++ algorithms/graph-theory/bfs-shortest-reach/bfs-shortest-reach.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import sys;
+from Queue import Queue
+
+def read_ints():
+    return [int (x) for x in sys.stdin.readline().split(" ")]
+
+def build_graph(N, M):
+    node_edges = [0]
+    while (N > 0):
+        node_edges.append([])
+        N -= 1
+    while (M > 0):
+        (x, y) = read_ints()
+        node_edges[x].append(y)
+        node_edges[y].append(x)
+        M -= 1
+    return node_edges
+
+def compute_distances(S, node_edges):
+    distances = {S:0}
+    queue = Queue()
+    queue.put(S)
+    while (not queue.empty()):
+        element = queue.get()
+        distance = distances[element] + 6
+        for neighbor in node_edges[element]:
+            if (neighbor in distances):
+                continue
+            distances[neighbor] = distance
+            queue.put(neighbor)
+    return distances
+
+def print_distances(S, N, distances):
+    for i in xrange(1, N + 1):
+        if (i == S):
+            continue;
+        if i in distances:
+            print distances[i],
+        else:
+            print -1,
+    print
+
+def test_case():
+    (N, M) = read_ints()
+    node_edges = build_graph(N, M)
+    S = int(sys.stdin.readline())
+    distances = compute_distances(S, node_edges)
+    print_distances(S, N, distances)
+
+T = int(sys.stdin.readline())
+
+while (T > 0):
+    test_case()
+    T -= 1

1fb3417338787095c33cde9316b1b2dc4160ddd2
Andrei Maximov <andrei.maximov@gmail.com>
2016-09-18 18:36:58 -0700

    Improve Minimum Window Substring challenge solution

--- leetcode/algorithms/minimum-window-substring/solution.py
+++ leetcode/algorithms/minimum-window-substring/solution.py
@@ -18,15 +18,15 @@ class Solution(object):
         Returns the minimum length window in the string that contains all the
         characters in the target.
         """
-        # The characters counts in the target string.
+        # The char counts in the target string.
         required = self._count(target)
 
-        # The counts of required characters in the current window.
+        # The counts of required chars in the current window.
         current = {c: 0 for c in required}
 
-        # The set of characters for which the count in the current window is
-        # >= the count of the character in the target.
-        fulfilled = set()
+        # The number of unique chars in the current dict that have a count
+        # >= to the count of the char in the target string.
+        fulfilled = 0
 
         window = ""
         start = 0
@@ -40,11 +40,11 @@ class Solution(object):
                 # If we have ran into c enough times add if to the fulfilled
                 # set of characters.
                 if current[c] == required[c]:
-                    fulfilled.add(c)
+                    fulfilled += 1
 
             # Slide forward the start of the window until we no longer have all
             # the characters in the target string.
-            while start <= i and len(fulfilled) == len(required):
+            while start <= i and fulfilled == len(required):
                 # Check if the this window is shorter than the current result.
                 length = i - start + 1
                 if len(window) == 0 or length < len(window):
@@ -56,8 +56,8 @@ class Solution(object):
                     current[r] -= 1
                     # Check if the character no longer occurs as many times as
                     # it does in the target string.
-                    if current[r] < required[r]:
-                        fulfilled.discard(r)
+                    if current[r] == required[r] - 1:
+                        fulfilled -= 1
 
                 # Slide the window forard.
                 start += 1

eb88133cb4a15b01ecf5a64a0243571f9a8f2b7e
Andrei Maximov <andrei.maximov@gmail.com>
2016-11-09 12:33:20 -0800

    Improve Word Break solution

--- leetcode/algorithms/word-break/solution.py
+++ leetcode/algorithms/word-break/solution.py
@@ -1,55 +1,7 @@
 #!/usr/bin/env python
 
-import bisect
-
 
 class Solution(object):
-    def _find(self, array, value):
-        """
-        Returns the index of the first occurence of value in array. The array
-        is expected to be sorted. Returns -1 if the value does not exist.
-        """
-        i = bisect.bisect_left(array, value)
-        if i != len(array) and array[i] == value:
-            return i
-        return -1
-
-    def _wordBreak(self, word, i, dictionary, cache):
-        """
-        Checks if word[i:] can be broken up into words from the specified
-        dictionary. Uses a list to cache results for all i from 0 to
-        len(word) - 1.
-        """
-        assert i >= 0
-        assert i < len(word)
-
-        # Check if we have solved this problem before.
-        if cache[i] is not None:
-            return cache[i]
-
-        # Check if the sub-string we are operating on (word[i:]) is in the
-        # dictionary.
-        full = word[i:]
-        if self._find(dictionary, full) != -1:
-            cache[i] = True
-            return True
-
-        # If the entire word[i:] sub-string is not in the dictionary, check if
-        # any of it's prefixes are and if the suffix after each such prefix
-        # can be broken down into words from the dictionary.
-        cache[i] = False
-        for j in range(i, len(word) - 1):
-            prefix = word[i:j + 1]
-            # Check if the prefix  is in the dictionary.
-            if self._find(dictionary, prefix) != -1:
-                # If the prefix is in the dictionary, check if the remaining
-                # suffix can be broken down into words.
-                if self._wordBreak(word, j + 1, dictionary, cache):
-                    cache[i] = True
-                    break
-
-        return cache[i]
-
     def wordBreak(self, word, dictionary):
         """
         Checks if the word can be broken up into words from the specified
@@ -59,12 +11,32 @@ class Solution(object):
         :type dictionary: List[str]
         :rtype: bool
         """
-        if len(word) == 0 or len(dictionary) == 0:
+        n = len(word)
+        if n == 0 or len(dictionary) == 0:
             return False
 
-        dictionary = list(sorted(dictionary))
-        cache = [None] * len(word)
-        return self._wordBreak(word, 0, dictionary, cache)
+        # Turn the dictionary into a set for ~O(1) lookup.
+        dictionary = set(dictionary)
+
+        # Each dp[i] indicates if we can word break word[:i]
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        # word[:i] can be broken up if (1) there exists a j such that word[:j]
+        # can be broken up and suffix word[j:i] is in the dictionary or (2)
+        # word[:i] is in the dictionary.
+        for i in range(1, n + 1):
+            # Find a prefix word[:j] that can be broken up. This includes the
+            # empty string to handle case 2.
+            for j in range(0, i):
+                if not dp[j]:
+                    continue
+                suffix = word[j:i]
+                if suffix in dictionary:
+                    dp[i] = True
+                    break
+
+        return dp[-1]
 
 
 def main():

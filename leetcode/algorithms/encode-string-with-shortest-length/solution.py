e49f990bd9ece457dc887e04eadbef783a84bb86
Andrei Maximov <andrei.maximov@gmail.com>
2016-12-12 19:44:59 -0800

    Fix Encode String with Shortest Length solution again

--- leetcode/algorithms/encode-string-with-shortest-length/solution.py
+++ leetcode/algorithms/encode-string-with-shortest-length/solution.py
@@ -5,59 +5,48 @@ class Solution(object):
     def encode(self, s):
         """Encodes a string with the shortest possible length in O(n^3) time.
 
-        See problem description for encoding rules.
+        See problem description for encoding rules. Credit to Stefan Pochmann
+        for finding optimal repeating prefix substring. See his original
+        solution at https://discuss.leetcode.com/topic/71442/short-python.
         """
         n = len(s)
 
-        # Each dp[i][j] reperesents the shortest encoding for s[i:j + 1].
-        dp = [[None for _ in range(n)] for _ in range(n)]
-        return self.encodeUtil(s, 0, n - 1, dp)
+        # Each dp[x] represents the shortest encoding for a substring x of s.
+        dp = dict()
+        return self.encodeUtil(s, dp)
 
-    def encodeUtil(self, s, i, j, dp):
-        """Calculates the shortest encoding for s[i:j + 1].
+    def encodeUtil(self, s, dp):
+        """Calculates the shortest encoding for s.
 
         There are O(n^2) subproblems but each subproblem considers O(n) smaller
         subproblems so the overall runtime is O(n^3)."""
-        n = j - i + 1
+        n = len(s)
         if n < 5:
             # Cannot compress anything shorter than 5 characters.
-            return s[i:j + 1]
-        elif dp[i][j] is not None:
-            return dp[i][j]
+            return s
+        elif s in dp:
+            return dp[s]
 
-        # Default encoding is just the raw string.
-        encoding = s[i:j + 1]
+        # Encode the string raw by default.
+        encoding = s
 
-        # Consider all repeating prefix substrings.
-        k = i
-        while k < j:
-            # Count how many times x occurs back to back in s[i:j + 1].
-            x = s[i:k + 1]
-            y = self.encodeUtil(s, i, k, dp)
-            count = 0
-            start = i
+        # Encode the string as the shortest repeating substring if possible.
+        i = (s + s).find(s, 1)
 
-            while s[start:start + len(x)] == x:
-                count += 1
-                start += len(x)
+        # Check if a repeating substring exists and it is not s.
+        if i != -1 and i < n:
+            repeat = s[:i]
+            count = n / len(repeat)
+            encoding = '%d[%s]' % (count, self.encodeUtil(repeat, dp))
 
-                # Compress the prefix.
-                prefix = y if count == 1 else '%d[%s]' % (count, y)
+        # Now consider all possible splits between chars 0 through n - 1.
+        for i in range(1, n):
+            left = self.encodeUtil(s[:i], dp)
+            right = self.encodeUtil(s[i:], dp)
+            if len(left) + len(right) < len(encoding):
+                encoding = left + right
 
-                # Compress the suffix.
-                suffix = self.encodeUtil(s, start, j, dp)
-
-                # Apply if encoding with x as prefix is shorter.
-                if len(prefix) + len(suffix) < len(encoding):
-                    encoding = prefix + suffix
-
-            # This is critical for speedup! Essentially if we have a prefix x
-            # that repeats p times, we do not need to consider any prefixes
-            # formed by repeating x p times. Why? Because without this we will
-            # be performing unecessary (repeat) calls to get optimal suffix.
-            k = i + len(x) * count
-
-        dp[i][j] = encoding
+        dp[s] = encoding
         return encoding
 
 

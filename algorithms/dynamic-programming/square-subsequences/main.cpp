58402c5e44f9860e8ef1a97199a99ea6e39fbb2d
Andrei Maximov <andrei.maximov@gmail.com>
2017-02-09 22:55:11 -0800

    Cleanup Square Subsequences problem

--- algorithms/dynamic-programming/square-subsequences/main.cpp
+++ algorithms/dynamic-programming/square-subsequences/main.cpp
@@ -1,97 +1,111 @@
 #include <iostream>
-#include <vector>
-#include <stdexcept>
+#include <array>
 
 const uint32_t MOD = 1000000007;
 
 const size_t MAX_LENGTH = 200;
 
-typedef std::vector<std::vector<uint32_t>> matrix;
-
-/**
- * The class substring presents an efficient substring view of a string by
- * using an offset and source string reference to avoid copy substrings.
- */
-class substring {
- private:
-  const std::string& source;
-
-  size_t start;
-
-  size_t end;
-
- public:
-  substring(const std::string& source, size_t start, size_t end) :
-    source(source), start(start), end(end) {
-  }
-
-  const char& operator[](size_t i) const {
-    if (i >= this->length()) {
-      throw std::invalid_argument("Out of bounds!");
-    }
-    return this->source[this->start + i];
-  }
-
-  size_t length() const {
-    return this->end - this->start + 1;
-  }
-};
-
-uint32_t add(const uint32_t& a, const uint32_t& b) {
-  return ((uint64_t) a + b) % MOD;
+//
+// Safely calculates (a + b) % MOD
+//
+int add(int a, int b) {
+  return ((int64_t) a + b) % MOD;
 }
 
-uint32_t sub(const uint32_t& a, const uint32_t& b) {
-  return ((uint64_t) a - b + MOD) % MOD;
+//
+// Safely calculates (a - b) % MOD
+//
+int sub(int a, int b) {
+  return ((int64_t) a - b + MOD) % MOD;
 }
 
-template<class S, class C>
-uint32_t common(const S& a, const S& b, C& cache) { // NOLINT
-  cache[0][0] = 1;
+//
+// Calculates the number of subsequences in common between L = s[0...p] and
+// R = s[p + 1...n] where each subsequence ends with s[p]. Takes
+// O(length(L) * length(R)) time.
+//
+int common(const std::string& s, int p) {
+  // Each dp[i][j] is the number of common subsequences in the first i
+  // characters of L and the first j characters of R.
+  int dp[MAX_LENGTH][MAX_LENGTH] = {{0}};
 
-  for (size_t j = 0; j <= a.length(); j++) {
-    for (size_t k = 0; k <= b.length(); k++) {
-      if (j > 0 || k > 0) {
-        cache[j][k] = 0;
-      }
-      if (j > 0) {
-        cache[j][k] = add(cache[j][k], cache[j - 1][k]);
-      }
-      if (k > 0) {
-        cache[j][k] = add(cache[j][k], cache[j][k - 1]);
-      }
-      if (j > 0 && k > 0 && a[j - 1] != b[k - 1]) {
-        cache[j][k] = sub(cache[j][k], cache[j - 1][k - 1]);
+  // Length of L = s[0...p].
+  int n = p + 1;
+
+  // Length of R = s[p + 1...n].
+  int m = s.size() - n;
+
+  // Base case - Length of longest common subsequences between "" and x where
+  // |x| > 0 is 1. (The empty subsequence)
+  for (int i = 0; i <= n; i++) {
+    dp[i][0] = 1;
+  }
+
+  for (int j = 0; j <= m; j++) {
+    dp[0][j] = 1;
+  }
+
+  //
+  // Consider function f(i, j) = The number of common subsequences between
+  // L[0...i - 1] and R[0...j - 1]. We have already described the base case
+  // H(*, 0) = H(0, *) = 1 above. The two recursive cases are as follows.
+  //
+  // 1. H(i, j) = H(i, j - 1) + H(i - 1, j) - H(i - 1, j - 1) if
+  //    L[i - 1] != R[j - 1]. We must subtract H(i - 1, j - 1) to avoid double
+  //    counting subsequences. (Inclusion Exclusion Principle)
+  //
+  // 2. H(i, j) = H(i, j - 1) + H(i - 1, j) - H(i - 1, j - 1) + H(i - 1, j - 1)
+  //    = H(i - 1, j) + H(i, j - 1) if L[i - 1] == R[j - 1]. We must consider
+  //    common subsequences counted by H(i - 1, j - 1) as a prefix to the
+  //    common subsequences ending with L[i - 1] == R[j - 1].
+  //
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      dp[i][j] = add(dp[i][j], dp[i - 1][j]);
+
+      dp[i][j] = add(dp[i][j], dp[i][j - 1]);
+
+      if (s[i - 1] != s[p + j]) {
+        dp[i][j] = sub(dp[i][j], dp[i - 1][j - 1]);
       }
     }
   }
 
-  uint32_t count = 0;
-  for (size_t k = 0; k < b.length(); k++) {
-    if (a[a.length() - 1] == b[k]) {
-      count = add(count, cache[a.length() - 1][k]);
+  //
+  // Count only the common subsequences ending with s[p] - the last character
+  // of L.
+  //
+  int count = 0;
+
+  for (int j = 0; j < m; j++) {
+    if (s[p] == s[p + j + 1]) {
+      count = add(count, dp[p][j]);
     }
   }
+
   return count;
 }
 
-uint32_t countss(const std::string& str, matrix& cache) { // NOLINT
-  uint32_t count = 0;
-  for (size_t i = 0; i < str.length() - 1; i++) {
-    substring a(str, 0, i);
-    substring b(str, i + 1, str.length() - 1);
-    count = add(count, common<substring, matrix>(a, b, cache));
+//
+// Counts the square subsequences in s. The idea behind the algorithm is to...
+//
+// 1. Consider all partitions s = LR of s where |L| > 0 and |R| > 0
+//
+// 2. Count the number of subsequences shared between each L and R ending with
+//    the last character of L. This is important because it forms a partition
+//    boundary so that we do not double count any subsequences across different
+//    partitions.
+//
+int countss(const std::string& s) {
+  int count = 0;
+  for (size_t p = 0; p < s.length() - 1; p++) {
+    count = add(count, common(s, p));
   }
   return count;
-}
-
-uint32_t countss(const std::string& str) {
-  matrix cache(MAX_LENGTH + 1, std::vector<uint32_t>(MAX_LENGTH + 1, 0));
-  return countss(str, cache);
 }
 
 int main() {
-  uint16_t T;
+  int T;
   std::string S;
   std::cin >> T;
   while (T--) {

0e46cbaa3f2826b6ff9d4ebd150b5e2330e66859
Andrei Maximov <andrei.maximov@gmail.com>
2018-02-11 15:33:30 -0800

    Implement alpha-beta pruning for Tic Tac Toe challenge

--- hacker-rank/artificial-intelligence/alpha-beta-pruning/tic-tac-toe/solution.py
+++ hacker-rank/artificial-intelligence/alpha-beta-pruning/tic-tac-toe/solution.py
@@ -2,81 +2,163 @@
 
 import sys
 
-# Map from max player -> values for winners. If X' is the max player, then
-# a tie with '_' winning is better than 'O' winning but worse than 'X' winning.
-VALUES = {'X': {'O': 0, '_': 1, 'X': 2},
-          'O': {'X': 0, '_': 1, 'O': 2}}
+
+class State:
+    def children(self):
+        '''Returns an iterator over child states.
+
+        NOTE: Valid only if NOT is_terminal().
+        '''
+        raise NotImplementedError()
+
+    def payoff(self):
+        '''Returns the payoff of the state.
+
+        NOTE: Valid only if is_terminal().
+        '''
+        raise NotImplementedError()
+
+    def payoff_lower(self):
+        '''Returns a lower bound on the payoff.'''
+        raise NotImplementedError()
+
+    def payoff_upper(self):
+        '''Returns an upper bound on the payoff.'''
+        raise NotImplementedError()
+
+    def is_terminal(self):
+        '''Checks if the state is terminal.'''
+        raise NotImplementedError()
+
+    def is_max_player(self):
+        '''Checks if the current state is a max player's turn.'''
+        raise NotImplementedError()
+
+
+class TicTacToe(State):
+    def __init__(self, board, player, is_max_player, move=None):
+        self._board = board
+        self._player = player
+        self._is_max_player = is_max_player
+        self._move = move
+
+    def children(self):
+        player = 'X' if self._player == 'O' else 'O'
+        is_max_player = not self._is_max_player
+
+        for r in range(3):
+            for c in range(3):
+                if self._board[r][c] == '_':
+                    board = [[x for x in row] for row in self._board]
+                    board[r][c] = self._player
+                    yield TicTacToe(board, player, is_max_player, (r, c))
+
+    def payoff(self):
+        winner = self._winner()
+
+        if winner is None:
+            return 0
+
+        # Either previous min-player won (-1) or previous max-player won (+1).
+        return -1 if self._is_max_player else 1
+
+    def payoff_lower(self):
+        return -1
+
+    def payoff_upper(self):
+        return 1
+
+    def is_terminal(self):
+        if self._winner() is not None:
+            return True
+
+        for r in range(3):
+            for c in range(3):
+                if self._board[r][c] == '_':
+                    return False
+
+        return True
+
+    def is_max_player(self):
+        return self._is_max_player
+
+    def move(self):
+        '''Returns the move used to transition to this state.'''
+        return self._move
+
+    def _winner(self):
+        '''Returns the current winner, if one exists.'''
+        board = self._board
+
+        for i in range(3):
+            # Check rows...
+            if board[i][0] != '_' and \
+               board[i][0] == board[i][1] == board[i][2]:
+                return board[i][0]
+
+            # Check columns...
+            if board[0][i] != '_' and \
+               board[0][i] == board[1][i] == board[2][i]:
+                return board[0][i]
+
+        # Check diagonals...
+        if board[0][0] != '_' and \
+           board[0][0] == board[1][1] == board[2][2]:
+            return board[0][0]
+
+        if board[2][0] != '_' and \
+           board[2][0] == board[1][1] == board[0][2]:
+            return board[2][0]
+
+        return None
 
 
 def main():
     player = raw_input()
     board = [list(raw_input()) for _ in range(3)]
-    r, c = minimax(board, player, player)[1]
-    print('%s %s' % (r, c))
+    state = TicTacToe(board, player, True)
+    state = alpha_beta_minimax_choice(state)
+    print('%d %d' % state.move())
 
 
-def minimax(board, curr_player, max_player):
-    '''Runs min-max on the board.'''
-    if finished(board):
-        w = winner(board)
-        return VALUES[max_player][w], None
+def alpha_beta_minimax_choice(state):
+    '''Uses alpha beta minimax to choose the best child state.'''
+    if state.is_terminal():
+        raise ValueError('Terminal state does not have children!')
+    elif not state.is_max_player():
+        raise ValueError('State should be a max player!')
 
-    if curr_player == 'X':
-        value, next_player = VALUES[max_player]['O'], 'O'
+    lower, upper = state.payoff_lower(), state.payoff_upper()
+    value, choice = lower, None
+
+    for s in state.children():
+        value_ = alpha_beta_minimax(s, lower, upper)
+        if value_ >= value:
+            value, choice = value_, s
+
+    return choice
+
+
+def alpha_beta_minimax(state, alpha, beta):
+    '''Runs alpha-beta pruning version of minimax on the state tree.'''
+    if state.is_terminal():
+        return state.payoff()
+    elif state.is_max_player():
+        value = state.payoff_lower()
+        for s in state.children():
+            value = max(value, alpha_beta_minimax(s, alpha, beta))
+            alpha = max(value, alpha)
+            if alpha >= beta:
+                break
+        return value
     else:
-        value, next_player = VALUES[max_player]['X'], 'X'
-
-    move = None
-
-    for r, c in options(board):
-        board[r][c] = curr_player
-        value_ = minimax(board, next_player, max_player)[0]
-        board[r][c] = '_'
-        if (curr_player == max_player and value_ >= value) or \
-           (curr_player != max_player and value_ <= value):
-            value, move = value_, (r, c)
-
-    return value, move
-
-
-def options(board):
-    '''Returns a generator of available moves.'''
-    for r in range(3):
-        for c in range(3):
-            if board[r][c] == '_':
-                yield (r, c)
-
-
-def finished(board):
-    '''Returns if the game is finished or not.'''
-    if winner(board) != '_':
-        return True
-    for r in board:
-        for x in r:
-            if x == '_':
-                return False
-    return True
-
-
-def winner(board):
-    '''Returns the current winner, if one exists.'''
-    for i in range(3):
-        # Check rows...
-        if board[i][0] != '_' and board[i][0] == board[i][1] == board[i][2]:
-            return board[i][0]
-        # Check cols...
-        if board[0][i] != '_' and board[0][i] == board[1][i] == board[2][i]:
-            return board[0][i]
-
-    # Check diags...
-    if board[0][0] != '_' and board[0][0] == board[1][1] == board[2][2]:
-        return board[0][0]
-
-    if board[2][0] != '_' and board[2][0] == board[1][1] == board[0][2]:
-        return board[2][0]
-
-    return '_'
-
+        value = state.payoff_upper()
+        for s in state.children():
+            value = min(value, alpha_beta_minimax(s, alpha, beta))
+            beta = min(beta, value)
+            if beta <= alpha:
+                break
+        return value
 
 if __name__ == '__main__':
     main()
